9f4797f7cb10694ff5b544e09c922d82a59aa975
Colin <Colin@DESKTOP-3OPQMAC>
2020-11-13 13:41:32 +0800

    init

---
+++ Colinapp.Cache/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Colinapp.Cache
+{
+    public class Class1
+    {
+    }
+}
---
+++ Colinapp.Data.EF/DbContext/SqlServerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+
+namespace Colinapp.Data.EF
+{
+    /// <summary>
+    /// sql数据库连接类
+    /// </summary>
+    public class SqlServerDbContext:DbContext,IDisposable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private string ConnectionString { get; set; }
+    }
+}
