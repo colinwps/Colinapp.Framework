c0e6dc31eff54c8e4be7bdb9b894fc4351bd906b
Colin <Colin@DESKTOP-3OPQMAC>
2020-11-30 10:19:50 +0800

    mysql init

--- Colinapp.Data.EF/DbContext/MySqlDbContext.cs
+++ Colinapp.Data.EF/DbContext/MySqlDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -14,6 +15,44 @@ namespace Colinapp.Data.EF
     /// </summary>
     public class MySqlDbContext : DbContext, IDisposable
     {
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        private string ConnectionString { get; set; }
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public MySqlDbContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        /// <summary>
+        /// 重载配置文件
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(ConnectionString, p => p.CommandTimeout(5000));
+            optionsBuilder.AddInterceptors(new DbCommandCustomInterceptor());
+        }
+        /// <summary>
+        /// 重载模型创建
+        /// </summary>
+        /// <param name="modelBuilder">模型构造器</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Assembly assembly = Assembly.Load(new AssemblyName("ColinApp.Application.Mapping"));
+            var typesToRegister = assembly.GetTypes()
+                .Where(type => !String.IsNullOrEmpty(type.Namespace))
+                .Where(type => type.BaseType != null && type.BaseType.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>));
+            foreach (var type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Model.AddEntityType(configurationInstance);
+            }
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
