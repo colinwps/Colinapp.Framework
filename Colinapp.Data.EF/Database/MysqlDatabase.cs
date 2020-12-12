496b4c1aaa72ae90515fbc08c804177856bbdcdc
admin <admin@ADMIN-PC>
2020-12-12 14:28:05 +0800

    web api init

---
+++ Colinapp.Data.EF/Database/MysqlDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colinapp.Data.EF.Database
+{
+    public class MysqlDatabase
+    {
+
+    }
+}
---
+++ Colinapp.Data.EF/Database/SqlServerDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colinapp.Data.EF.Database
+{
+    public class SqlServerDatabase
+    {
+    }
+}
